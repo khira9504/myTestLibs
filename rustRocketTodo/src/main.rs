4ac7ec33808644041001b41b2d7015cffa9af2ff
kh9504 <k.hiraizumi0417@gmail.com>
2025-03-10 00:05:29 +0900

    修正

--- rustRocketTodo/src/main.rs
+++ rustRocketTodo/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+// getルートのハンドラーを示すマクロ
+#[get("/")]
+// index関数の返り値が文字列を示す
+fn index() -> &'static str {
+  "Hello, world from Rocket!"
+}
+
+// このlaunchアトリビュート付きのrocket関数はアプリのエンドポイント
+#[launch]
+fn rocket() -> _ {
+  rocket::build().mount("/", routes![index])
 }
