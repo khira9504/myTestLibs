99de36d25a213c8ac068ea4e0060399aa52f71f1
kh9504 <k.hiraizumi0417@gmail.com>
2025-03-23 10:37:51 +0900

    fix

--- pythonWebApp/main.py
+++ pythonWebApp/main.py
@@ -1,7 +1,32 @@
 import streamlit as st
 
 def main():
-  st.write("Hello World!!")
+  st.write("Counter Example")
+  # 初期化されているかどうかの確認
+  # 初期化されている場合は0を代入し、そうでない場合はreturnする
+  if "count" not in st.session_state:
+    st.session_state.count = 0
+  
+  # incrementボタンを反映
+  increment = st.button("increment")
+  # incrementボタンが押されたら+１
+  if increment:
+    st.session_state.count += 1
+
+  # decrementボタンを反映
+  decrement = st.button("decrement")
+  # decrementボタンが押されたら-１
+  if decrement:
+    st.session_state.count -= 1
+
+  # resetボタンを反映
+  reset = st.button("reset")
+  # resetボタンが押されたら０
+  if reset:
+    st.session_state.count = 0
+
+  # カウント数を表示
+  st.write("Count = ", st.session_state.count)
 
 if __name__ == "__main__":
   main()
