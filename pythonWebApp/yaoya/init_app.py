fbbb6552026c5e2bc2ca4af4ed4d93d00467ccc5
kh9504 <k.hiraizumi0417@gmail.com>
2025-03-27 00:28:05 +0900

    add

---
+++ pythonWebApp/yaoya/init_app.py
@@ -0,0 +1,19 @@
+def init_session() -> StreamlitSessionManager:
+  mockdir = Path(TemporaryDirectory().name)
+  mockdir.mkdir(exist_ok=True)
+  mockdb = MockDB(mockdir.joinpath("mock.db"))
+  session_db = MockSessionDB(mockdir.joinpath("session.json"))
+  ssm = StreamlitSessionManager(
+
+  )
+  return ssm
+
+def init_pages(ssm: StreamlitSessionManager) -> list[BasePage]:
+  pages = [
+    # ページクラスを追加
+  ]
+  return pages
+
+def init_app(ssm: StreamlitSessionManager, pages: list[BasePage]) -> MultiPageApp:
+  app = MultiPageApp(ssm, pages)
+  return app
