3c057ff18a6d5ac2c69d8036793d704b2733898b
kh9504 <k.hiraizumi0417@gmail.com>
2025-03-29 19:01:19 +0900

    fix

--- pythonWebApp/yaoya/init_app.py
+++ pythonWebApp/yaoya/init_app.py
@@ -1,23 +1,3 @@
-from pathlib import Path
-from tempfile import TemporaryDirectory
-
-from yaoya.app import MultiPageApp
-from yaoya.const import PageId
-from yaoya.pages.base import BasePage
-from yaoya.pages.member.cart import CartPage
-from yaoya.pages.member.order_detail import OrderDetailPage
-from yaoya.pages.member.order_list import OrderListPage
-from yaoya.pages.public.item_detail import ItemDetailPage
-from yaoya.pages.public.item_list import ItemListPage
-from yaoya.pages.public.login import LoginPage
-from yaoya.services.auth import MockAuthAPIClientService
-from yaoya.services.cart import MockCartAPIClientService
-from yaoya.services.item import MockItemAPIClientService
-from yaoya.services.mock import MockDB, MockSessionDB
-from yaoya.services.order import MockOrderAPIClientService
-from yaoya.services.user import MockUserAPIClientService
-from yaoya.sesseion import StreamlitSessionManager
-
 def init_session() -> StreamlitSessionManager:
   mockdir = Path(TemporaryDirectory().name)
   mockdir.mkdir(exist_ok=True)
