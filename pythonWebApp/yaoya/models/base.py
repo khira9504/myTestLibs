d9bb818ab8d163301793d64e2444dfb208e381db
kh9504 <k.hiraizumi0417@gmail.com>
2025-03-30 18:28:03 +0900

    add

---
+++ pythonWebApp/yaoya/models/base.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from typing import Protocol
+
+class BaseDataModel(Protocol):
+  def to_dict(self) -> dict[str, str]:
+    pass
+
+  @classmethod
+  def from_dict(cls, data: dict[str, str]) -> BaseDataModel:
+    pass
