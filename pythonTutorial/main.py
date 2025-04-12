52137b2f119c557691fd6e67aba90dda1788b766
kh9504 <k.hiraizumi0417@gmail.com>
2025-04-12 10:22:45 +0900

    add

--- pythonTutorial/main.py
+++ pythonTutorial/main.py
@@ -6,3 +6,13 @@ print('C:\some\name')
 # 引用符の前 r に注目
 print(r'C:\some\name')
 # C:\some\name
+
+# 0番目(含む)から2番目(含めない)までの文字
+word = 'Python'
+print(word[0:2])
+# Py
+
+# 文字列の長さを測る
+s = 'supercalifragilisticexpialidocious'
+print(len(s))
+# 34
